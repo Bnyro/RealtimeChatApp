ab248f59a3dc7a23ed4109aee6c1f2b4dd56521e
Bnyro <bnyro@tutanota.com>
2022-12-22 19:42:59 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/rtc
+
+go 1.19
+
+require github.com/gofiber/fiber/v2 v2.40.1
+
+require (
+	github.com/fasthttp/websocket v1.5.0 // indirect
+	github.com/savsgio/gotils v0.0.0-20211223103454-d0aaa54c5899 // indirect
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/gofiber/websocket/v2 v2.1.2
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.41.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+)
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"sync"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+)
+
+type client struct {
+	isClosing bool
+	mu        sync.Mutex
+}
+
+var clients = make(map[*websocket.Conn]*client)
+var register = make(chan *websocket.Conn)
+var broadcast = make(chan string)
+var unregister = make(chan *websocket.Conn)
+
+// Run the WebSocket hub
+func runHub() {
+	for {
+		select {
+		case connection := <-register:
+			clients[connection] = &client{}
+			log.Println("connection registered")
+
+		case message := <-broadcast:
+			log.Println("message received:", message)
+			// Send the message to all clients in parallel
+			for connection, c := range clients {
+				go func(connection *websocket.Conn, c *client) {
+					c.mu.Lock()
+					defer c.mu.Unlock()
+					if c.isClosing {
+						return
+					}
+					if err := connection.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
+						c.isClosing = true
+						log.Println("write error:", err)
+
+						connection.WriteMessage(websocket.CloseMessage, []byte{})
+						connection.Close()
+						unregister <- connection
+					}
+				}(connection, c)
+			}
+
+		case connection := <-unregister:
+			// Remove the client from the hub
+			delete(clients, connection)
+
+			log.Println("connection unregistered")
+		}
+	}
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Static("/", "./static")
+
+	app.Use(func(c *fiber.Ctx) error {
+		// Returns true if the client requested upgrade to the WebSocket protocol
+		if websocket.IsWebSocketUpgrade(c) {
+			return c.Next()
+		}
+		return c.SendStatus(fiber.StatusUpgradeRequired)
+	})
+
+	go runHub()
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		// When the function returns, unregister the client and close the connection
+		defer func() {
+			unregister <- c
+			c.Close()
+		}()
+
+		// Register the client
+		register <- c
+
+		for {
+			messageType, message, err := c.ReadMessage()
+			if err != nil {
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					log.Println("read error:", err)
+				}
+
+				return // Calls the deferred function, i.e. closes the connection on error
+			}
+
+			if messageType == websocket.TextMessage {
+				// Broadcast the received message
+				broadcast <- string(message)
+			} else {
+				log.Println("websocket message received of type", messageType)
+			}
+		}
+	}))
+
+	addr := flag.String("addr", ":8080", "http service address")
+	flag.Parse()
+	log.Fatal(app.Listen(*addr))
+}
