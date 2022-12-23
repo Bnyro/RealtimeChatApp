2dd57d936e67405fecfbffdb1022e3556b598843
Bnyro <bnyro@tutanota.com>
2022-12-23 15:38:41 +0100

    Improve the JS name submission

--- static/app.js
+++ static/app.js
@@ -1,11 +1,21 @@
 let $ = document.querySelector.bind(document);
 let conn;
 
+class Message {
+  constructor(author, text) {
+    this.author = author;
+    this.text = text;
+  }
+
+  static from(json) {
+    return Object.assign(new Message(), json);
+  }
+}
+
 window.onload = function () {
   function appendLog(item) {
     var doScroll =
-      $("#log").scrollTop >
-      $("#log").scrollHeight - $("#log").clientHeight - 1;
+      $("#log").scrollTop > $("#log").scrollHeight - $("#log").clientHeight - 1;
     $("#log").appendChild(item);
     if (doScroll) {
       $("#log").scrollTop = $("#log").scrollHeight - $("#log").clientHeight;
@@ -14,7 +24,11 @@ window.onload = function () {
 
   $("#form").onsubmit = function () {
     if (!conn || !$("#msg").value || !$("#name").value) return false;
-    conn.send(`${$("#name").value.replace(":", "")}: ${$("#msg").value}`);
+    let message = new Message(
+      $("#name").value.replace(":", ""),
+      $("#msg").value
+    );
+    conn.send(JSON.stringify(message));
     $("#msg").value = "";
     return false;
   };
@@ -22,23 +36,21 @@ window.onload = function () {
   if (window["WebSocket"]) {
     conn = new WebSocket("ws://" + document.location.host + "/ws");
     conn.onclose = function (evt) {
-      var item = document.createElement("div");
+      const item = document.createElement("div");
       item.innerHTML = "<b>Connection closed.</b>";
       appendLog(item);
     };
     conn.onmessage = function (evt) {
-      var messages = evt.data.split("\n");
-      for (var i = 0; i < messages.length; i++) {
-        var container = document.createElement("div");
-        var name = document.createElement("span");
-        name.innerText = messages[i].split(": ")[0] + ": ";
-        container.appendChild(name);
-        container.innerHTML += messages[i].split(": ")[1];
-        appendLog(container);
-      }
+      const message = Message.from(JSON.parse(evt.data));
+      const container = document.createElement("div");
+      const name = document.createElement("span");
+      name.innerText = message.author + ": ";
+      container.appendChild(name);
+      container.innerHTML += message.text;
+      appendLog(container);
     };
   } else {
-    var item = document.createElement("div");
+    const item = document.createElement("div");
     item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
     appendLog(item);
   }
